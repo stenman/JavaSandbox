d08b8345ca5abd65d5e7508156704655306b441f
stenman <roger.stenman@gmail.com>
2017-02-19 00:13:58 +0100

    just added some old stuff

--- src/com/example/sandbox/mains/ExtendMe.java
+++
@@ -1,6 +0,0 @@
-package com.example.sandbox.mains;
-
-public interface ExtendMe {
-
-    String toString();
-}
--- src/com/example/sandbox/stuff/GenericsBox.java
+++
@@ -1,8 +0,0 @@
-package com.example.sandbox.stuff;
-
-public class GenericsBox {
-    public <T> T doStuff(T a1) {
-        // Here we can do stuff to a1
-        return a1;
-    }
-}
