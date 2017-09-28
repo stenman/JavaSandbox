718012c0c1de7a13fdd46de74aa00b129764453f
stenman <roger.stenman@gmail.com>
2017-09-28 21:19:05 +0200

    more jna experiment

--- src/main/java/com/example/sandbox/mains/GeneralTest.java
+++ src/main/java/com/example/sandbox/mains/GeneralTest.java
@@ -1,12 +1,27 @@
 package com.example.sandbox.mains;
 
+import com.example.sandbox.stuff.Thing;
+
 import java.io.IOException;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import static java.util.Arrays.asList;
 
 public class GeneralTest {
 
-	private final static String PRINT = "hej"; 
-	
-	public static void main(String[] args) throws IOException {
-			System.out.println(PRINT);
-	}
+    public static void main(String[] args) throws IOException {
+        List<Thing> things = asList(new Thing(1, "a"), new Thing(2, "b"), new Thing(3, "c"));
+
+        Set<Integer> thingsToRemove = new HashSet<>();
+        thingsToRemove.add(2);
+
+        things.removeIf(t -> thingsToRemove.contains(t.getId()));
+
+        System.out.println(things);
+        System.out.println(thingsToRemove);
+
+
+    }
 }
