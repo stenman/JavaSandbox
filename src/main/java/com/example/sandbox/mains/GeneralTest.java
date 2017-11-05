25363f4428bde43485d45df6b9aac16ce9d0fb77
stenman <roger.stenman@gmail.com>
2017-11-05 15:43:23 +0100

    random stuff

--- src/main/java/com/example/sandbox/mains/GeneralTest.java
+++ src/main/java/com/example/sandbox/mains/GeneralTest.java
@@ -1,57 +1,37 @@
 package com.example.sandbox.mains;
 
-import com.example.sandbox.stuff.Thing;
-
 import java.io.IOException;
+import java.text.NumberFormat;
 import java.util.ArrayList;
-import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.List;
-import java.util.Set;
-import java.util.function.Consumer;
-import java.util.function.Predicate;
-
-import static java.util.Arrays.asList;
+import java.util.Locale;
 
 public class GeneralTest {
 
     public static void main(String[] args) throws IOException {
+        List<Integer> integersArray = new ArrayList<>();
 
-//        List<Thing> list = asList(new Thing(1, "Mahesh"), new Thing(2, "Ram"), new Thing(3, "Krishna"));
-//        list.add(new Thing(1, "Mahesh"));
-//        list.add(new Thing(2, "Ram"));
-//        list.add(new Thing(3, "Krishna"));
-//        Consumer<Thing> style = (Thing p) -> System.out.println("id:"+p.getId() +", Name:"+p.getName());
-//        System.out.println("---Before delete---\n"+list);
-//        list.forEach(style);
-//        Predicate<Thing> personPredicate = p-> p.getId() == 2;
-//        list.removeIf(personPredicate);
-//        System.out.println("---After delete---\n"+list);
-//        list.forEach(style);
+        for (int i = 0; i < 10000000; i++) {
+            integersArray.add(i);
+            if (i % 1000000 == 0) {
+                System.out.println(i);
+            }
+        }
+        System.out.println("Size: " + NumberFormat.getNumberInstance(Locale.US).format(integersArray.size()));
+        System.out.println("Get element: " + integersArray.get(5000000));
+        System.out.println("Now, let's try a linked list!");
 
-        List<Thing> things = new ArrayList<>(asList(new Thing(1, "a"), new Thing(2, "b"), new Thing(3, "c")));
+        List<Integer> integersLinked = new LinkedList<>();
 
-        List<Thing> things1 = new ArrayList<>();
-        Thing t1 = new Thing(1, "a");
-        Thing t2 = new Thing(2, "b");
-        Thing t3 = new Thing(3, "c");
-        things1.addAll(asList(t1, t2, t3));
-
-        Consumer<Thing> printStyle = (Thing t) -> System.out.println("id:" + t.getId() + ", Name:" + t.getName());
-
-        System.out.println("---Before removal---");
-        things.forEach(printStyle);
-        things1.forEach(printStyle);
-
-        Set<Integer> thingsToRemove = new HashSet<>();
-        thingsToRemove.add(2);
-
-        things.removeIf(t -> thingsToRemove.contains(t.getId()));
-        things1.removeIf(t -> t.getId() == 2);
-
-        System.out.println("---After removal---");
-        things.forEach(printStyle);
-        things1.forEach(printStyle);
-
+        for (int i = 0; i < 10000000; i++) {
+            integersLinked.add(i);
+            if (i % 1000000 == 0) {
+                System.out.println(i);
+            }
+        }
+        System.out.println("Size: " + NumberFormat.getNumberInstance(Locale.US).format(integersLinked.size()));
+        System.out.println("Get element: " + integersLinked.get(5000000));
 
     }
 }
--- src/main/java/com/example/sandbox/mains/StreamsExperiment.java
+++ src/main/java/com/example/sandbox/mains/StreamsExperiment.java
@@ -1,8 +1,13 @@
 package com.example.sandbox.mains;
 
+import com.example.sandbox.stuff.Thing;
+
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Consumer;
 
+import static java.util.Arrays.asList;
 import static java.util.stream.Collectors.toList;
 
 /**
@@ -11,16 +16,30 @@ import static java.util.stream.Collectors.toList;
 public class StreamsExperiment {
 
     public static void main(String args[]) {
+        printStyle();
+        manipulateIntegers();
+    }
 
+    private static void printStyle() {
+        List<Thing> things = new ArrayList<>(asList(new Thing(1, "a"), new Thing(2, "b"), new Thing(3, "c")));
+        print(things);
+    }
+
+    private static void manipulateIntegers() {
         List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 2, 3, 4, 5, 6);
 
-        List<Integer> hej =
+        List<Integer> numbersToPrint =
                 numbers.stream()
                         .filter(integer -> integer > 3)
                         .filter(integer -> integer % 2 == 0)
                         .map(integer -> integer * 2)
                         .collect(toList());
-        System.out.println(hej);
+        System.out.println(numbersToPrint);
+    }
+
+    private static void print(List list) {
+        Consumer<Thing> printStyle = (Thing t) -> System.out.println("id:" + t.getId() + ", Name:" + t.getName());
+        list.forEach(printStyle);
     }
 
 
